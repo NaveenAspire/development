a5a1a3b5cc3678d352eaf8c7ddc014c1659934c4
NaveenAspire <naveenkumar.mani@aspiresys.com>
2022-04-27 18:40:33 +0530

    Updated Apr-27

--- pull_data_from_meta_weather_api/metaweather_api.py
+++ pull_data_from_meta_weather_api/metaweather_api.py
@@ -2,9 +2,10 @@ import requests
 class MetaWeatherApi:
     """This class has the methods for each endpoints in the meta weather api"""
 
-    def __init__(self) -> None:
+    def __init__(self,logger_obj) -> None:
         """This is the init method of the class MeataWeatherApi"""
-        pass
+        self.logging = logger_obj
+        self.logging.info("Object created successfully for the class MetaWeatherApi")
 
     def weather_information_using_woeid_date(self, woeid, date):
         """This method will provide the weather information based on woeid and date
@@ -12,6 +13,7 @@ class MetaWeatherApi:
         try:
             endpoint = "https://www.metaweather.com/api/location/{woeid}/{date}/".format(woeid=woeid,date=date)
             response = requests.get(endpoint).json()
+            self.logging.info("Weather information sucessfully retrieved for{date} and {woeid}".format(date=date,woeid=woeid))
         except Exception as err:
             print(err)
             response = None
--- pull_data_from_meta_weather_api/pull_weather_information_from_metaweather_api.py
+++ pull_data_from_meta_weather_api/pull_weather_information_from_metaweather_api.py
@@ -9,17 +9,18 @@ import json
 import logging
 import os
 import sys
-# from dummy_S3.dummy_s3 import DummyS3
+from this import d
+
+import pandas as pd
+from dummy_S3.dummy_s3 import DummyS3
 from metaweather_api import MetaWeatherApi
 from S3.s3 import S3Service
 
 parent_dir = os.path.dirname(os.getcwd())
-print(parent_dir)
 config = configparser.ConfigParser()
 config.read(parent_dir+"/develop.ini")
 log_dir = os.path.join(parent_dir,
                         'opt/logging/pull_weather_information_from_metaweather_api/')
-print(log_dir)
 os.makedirs(log_dir,exist_ok=True)
 log_file = os.path.join(log_dir,'weather_information.log')
 
@@ -43,18 +44,29 @@ class PullWeatherInformationFromMetaWeatherApi:
         self.s_date = s_date
         self.e_date = e_date
         self.path = os.path.join(os.getcwd(), config["local"]["local_file_path"])
-        self.metaweather = MetaWeatherApi()
+        self.metaweather = MetaWeatherApi(logger)
         logger.info("Object successfully created!!!")
 
     def get_weather_information_for_given_dates(self):
         """This method will get the weather information from start date to end date"""
+        last_run = config['pull_weather_information_from_metaweather_api']['last_date']
+        if last_run:
+            last_run = datetime.strptime(last_run,'%Y-%m-%d').date()
         if self.e_date:
-            for n in range(int((self.e_date - self.s_date).days) + 1):
-                date = self.s_date + timedelta(n)
-                print(date)
-                self.get_weather_information(date)
+            logging.info("The script retrieves weather information for specific two dates")
+            self.get_weather_information_between_two_dates(self.s_date,self.e_date)
+        elif last_run < self.s_date:
+            logging.info("The script retrieves weather information for last day of script run to yesterdday")
+            self.get_weather_information_between_two_dates(last_run,self.s_date)
         else:
+            logging.info("The script retrieves weather information for yesterday")
             self.get_weather_information(self.s_date)
+    
+    def get_weather_information_between_two_dates(self,start,end):
+        """This method help to collect the weather information between two dates"""
+        for n in range(int((end - start).days) + 1):
+                date = start + timedelta(n)
+                self.get_weather_information(date)
 
     def get_weather_information(self, date):
         """This method used to get the woeid of city from api"""
@@ -64,21 +76,31 @@ class PullWeatherInformationFromMetaWeatherApi:
             if response == None:
                 sys.exit()
             self.get_given_date_response(response, key, date)
-        return response
+            return response
 
     def get_given_date_response(self, response, city, date):
         """This method used to get only required date of information alone"""
         date = date.strftime("%Y-%m-%d")
-        for information in response:
-            if date in information["created"]:
-                with open(self.path + city + "_" + information["created"] + ".json", "w") as file:
-                    json.dump(information, file)
-                    print("successfully created json file in local")
+        df = pd.DataFrame(response)
+        for i in range(2):
+            t_str = '{date}T{hour}'.format(date=date,hour=str(i).zfill(2))
+            print(t_str)
+            new_df = df[(df.created.str.contains(t_str))]
+            print(new_df.created)
+        # cre = df.created[0].split(':')[0]
+        # new_df = df[(df.created.str.contains(cre))]
+        # print(cre)
+        # for information in response:
+        #     if date in information["created"]:
+        #         with open(self.path + city + "_" + information["created"] + ".json", "w") as file:
+        #             json.dump(information, file)
+        #             print("successfully created json file in local")
+        #             logging.info("The script retrieved weather information for "+date)
         return True
 
     def upload_to_s3(self):
         """This method used to upload the file to s3 which data got from api"""
-        s3_service = S3Service()
+        s3_service = S3Service(logger)
         for file in os.listdir(self.path):
             key = self.get_partition_path(file) + file
             response = s3_service.upload_file(self.path + file, key)
@@ -117,11 +139,8 @@ def main():
         help="Enter date for pull data",
     )
     args = parser.parse_args()
-    print(type(args.s_date))
-    args.s_date = "fsfs"
-    print(args.s_date)
-    # pull_data = PullWeatherInformationFromMetaWeatherApi(args.s_date, args.e_date)
-    # pull_data.get_weather_information_for_given_dates()
+    pull_data = PullWeatherInformationFromMetaWeatherApi(args.s_date, args.e_date)
+    pull_data.get_weather_information_for_given_dates()
     # pull_data.upload_to_s3()
     
     # dummy_s3 = DummyS3()
@@ -130,12 +149,11 @@ def main():
 def set_last_run():
     """This function that will set the last run of the script"""
     config.set('pull_weather_information_from_metaweather_api','last_date',str(datetime.now().date()))
-    print(config.get("pull_weather_information_from_metaweather_api","last_date"))
-    with open(parent_dir+'develop.ini', 'w') as file:
+    with open(parent_dir+'/develop.ini', 'w') as file:
         config.write(file)
         
     
 if __name__ == "__main__":
     main()
-    # set_last_run()
+    set_last_run()
     # print("sdfsd")
