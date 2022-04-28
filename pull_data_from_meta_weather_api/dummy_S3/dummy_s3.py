3b507c97f257100f12786f2008b094ae0045f6a3
NaveenAspire <naveenkumar.mani@aspiresys.com>
2022-04-28 15:14:23 +0530

    Updated

--- pull_data_from_meta_weather_api/dummy_S3/dummy_s3.py
+++ pull_data_from_meta_weather_api/dummy_S3/dummy_s3.py
@@ -8,12 +8,12 @@ class DummyS3:
     def __init__(self,config_obj) -> None:
         """This is the init method of the class of DummyS3"""
         self.config = config_obj
-    def upload_dummy_local_s3(self):
-        for file in os.listdir(self.path):
-            partition_path = self.get_partition_path(file)
-            local_s3_path = self.config["local"]["local_s3_path"]
-            dummy_s3 = local_s3_path + partition_path
-            print(dummy_s3)
-            os.makedirs(dummy_s3, exist_ok=True)
-            shutil.copy(self.path + file, dummy_s3)
+        local_s3_path = self.config["local"]["local_s3_path"]
+        self.dummy_s3_path = os.path.join(os.path.dirname(os.getcwd()),local_s3_path)
+    def upload_dummy_local_s3(self,source,partition_path):  
+        dest = self.dummy_s3_path + partition_path 
+        os.makedirs(dest, exist_ok=True)
+        shutil.copy(source,dest)
+        return True
+            
     
--- pull_data_from_meta_weather_api/pull_weather_information_from_metaweather_api.py
+++ pull_data_from_meta_weather_api/pull_weather_information_from_metaweather_api.py
@@ -1,15 +1,12 @@
-"""This module that pull weather information from the meat weather api 
+"""This module that pull weather information from the meat weather api
 as json file and upload to in s3 based on partition city,year,month,day and hour."""
 import ast
-import re
 import configparser
 from datetime import datetime, timedelta
 import argparse
-import json
 import logging
 import os
 import sys
-from this import d
 from time import time
 
 import pandas as pd
@@ -19,11 +16,10 @@ from S3.s3 import S3Service
 
 parent_dir = os.path.dirname(os.getcwd())
 config = configparser.ConfigParser()
-config.read(parent_dir+"/develop.ini")
-log_dir = os.path.join(parent_dir,
-                        'opt/logging/pull_weather_information_from_metaweather_api/')
-os.makedirs(log_dir,exist_ok=True)
-log_file = os.path.join(log_dir,'weather_information.log')
+config.read(parent_dir + "/develop.ini")
+log_dir = os.path.join(parent_dir, "opt/logging/pull_weather_information_from_metaweather_api/")
+os.makedirs(log_dir, exist_ok=True)
+log_file = os.path.join(log_dir, "weather_information.log")
 
 logging.basicConfig(
     filename=log_file,
@@ -33,6 +29,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
 
 class PullWeatherInformationFromMetaWeatherApi:
     """This is the class have methods for pull weather infromation from the metaweather api"""
@@ -44,73 +41,72 @@ class PullWeatherInformationFromMetaWeatherApi:
         )
         self.s_date = s_date
         self.e_date = e_date
-        self.path = os.path.join(parent_dir, config["local"]["local_file_path"],'meta_weather')
-        os.makedirs(self.path,exist_ok=True)
+        self.path = os.path.join(parent_dir, config["local"]["local_file_path"], "meta_weather")
+        os.makedirs(self.path, exist_ok=True)
         self.metaweather = MetaWeatherApi(logger)
         logger.info("Object successfully created!!!")
 
     def get_weather_information_for_given_dates(self):
         """This method will get the weather information from start date to end date"""
-        last_run = config['pull_weather_information_from_metaweather_api']['last_date']
-        if last_run:
-            last_run = datetime.strptime(last_run,'%Y-%m-%d').date()
-        if self.e_date:
-            logging.info("The script retrieves weather information for specific two dates")
-            self.get_weather_information_between_two_dates(self.s_date,self.e_date)
-        elif last_run < self.s_date:
-            logging.info("The script retrieves weather information for last day of script run to yesterdday")
-            self.get_weather_information_between_two_dates(last_run,self.s_date)
-        else:
-            logging.info("The script retrieves weather information for yesterday")
+        last_run = config["pull_weather_information_from_metaweather_api"]["last_date"]
+        if last_run and self.s_date == self.e_date and get_date(last_run) < self.s_date:
+            last_run = datetime.strptime(last_run, "%Y-%m-%d").date()
+            self.s_date = last_run
+        while self.s_date <= self.e_date:
             self.get_weather_information(self.s_date)
-    
-    def get_weather_information_between_two_dates(self,start,end):
-        """This method help to collect the weather information between two dates"""
-        for n in range(int((end - start).days) + 1):
-                date = start + timedelta(n)
-                self.get_weather_information(date)
+            self.s_date = self.s_date + timedelta(1)
 
     def get_weather_information(self, date):
         """This method used to get the woeid of city from api"""
         search_date = date.strftime("%Y/%m/%d")
         for key, value in self.woeid_date.items():
             response = self.metaweather.weather_information_using_woeid_date(value, search_date)
-            if response == None:
+            if response is None:
                 sys.exit()
             self.get_given_date_response(response, key, date)
-            return response
+        return response
 
     def get_given_date_response(self, response, city, date):
         """This method used to get only required date of information alone"""
         date = date.strftime("%Y-%m-%d")
-        df = pd.DataFrame(response)
-        for i in range(2):
-            t_str = '{date}T{hour}'.format(date=date,hour=str(i).zfill(2))
-            new_df = df[(df.created.str.contains(t_str))]
+        res_df = pd.DataFrame(response)
+        for i in range(24):
+            t_str = "{date}T{hour}".format(date=date, hour=str(i).zfill(2))
+            new_df = res_df[(res_df.created.str.contains(t_str))]
             if not new_df.empty:
                 epoch = int(time())
-                file_name = 'metaweather_{city}_{epoch}.json'.format(city=city,epoch=epoch)
-                new_df.to_json(self.path+'/'+file_name,orient='records',lines=True)
+                file_name = "metaweather_{city}_{epoch}.json".format(city=city, epoch=epoch)
+                new_df.to_json(self.path + "/" + file_name, orient="records", lines=True)
+                # key = self.get_partition_path(city,t_str)+'/'+file_name
+                # self.upload_to_s3(self.path+'/'+file_name,key)
+                self.upload_to_dummy_s3(
+                    self.path + "/" + file_name, self.get_partition_path(city, t_str)
+                )
         return True
 
-    def upload_to_s3(self):
+    def upload_to_s3(self, file, key):
         """This method used to upload the file to s3 which data got from api"""
         s3_service = S3Service(logger)
-        for file in os.listdir(self.path):
-            key = self.get_partition_path(file) + file
-            response = s3_service.upload_file(self.path + file, key)
+        response = s3_service.upload_file(file, key)
         return response
 
-    def get_partition_path(self, file):
+    def upload_to_dummy_s3(self, source, partition_path):
+        """This method used to upload the file to dummy s3 which data got from api"""
+        dummy_s3 = DummyS3(config)
+        response = dummy_s3.upload_dummy_local_s3(source, partition_path)
+        return response
+
+    def get_partition_path(self, city, t_str):
         """This method will make key name for uploading into s3 with partition"""
-        pt_values = re.split("_|-|T|:", file)
-        partition_path = "pt_city={city_name}/pt_year={year}/pt_month={month}/pt_day={day}/pt_hour={hour}/".format(
-            city_name=pt_values[0],
-            year=pt_values[1],
-            month=pt_values[2],
-            day=pt_values[3],
-            hour=pt_values[4],
-        )
+        try:
+            date_obj = datetime.strptime(t_str, "%Y-%m-%dT%H")
+            partition_path = date_obj.strftime(
+                "pt_city={city_name}/pt_year=%Y/pt_month=%m/pt_day=%d/pt_hour=%H/".format(
+                    city_name=city
+                )
+            )
+        except Exception as err:
+            print(err)
         return partition_path
 
 
@@ -132,22 +128,22 @@ def main():
         "--e_date",
         type=get_date,
         help="Enter date for pull data",
+        default=datetime.now().date() - timedelta(1),
     )
     args = parser.parse_args()
     pull_data = PullWeatherInformationFromMetaWeatherApi(args.s_date, args.e_date)
     pull_data.get_weather_information_for_given_dates()
-    # pull_data.upload_to_s3()
-    
-    # dummy_s3 = DummyS3()
-    # dummy_s3.upload_dummy_local_s3()
+
 
 def set_last_run():
     """This function that will set the last run of the script"""
-    config.set('pull_weather_information_from_metaweather_api','last_date',str(datetime.now().date()))
-    with open(parent_dir+'/develop.ini', 'w') as file:
+    config.set(
+        "pull_weather_information_from_metaweather_api", "last_date", str(datetime.now().date())
+    )
+    with open(parent_dir + "/develop.ini", "w",encoding='utf-8') as file:
         config.write(file)
-        
-    
+
+
 if __name__ == "__main__":
     main()
     set_last_run()
