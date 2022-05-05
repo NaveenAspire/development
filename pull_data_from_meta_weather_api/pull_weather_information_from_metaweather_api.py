4f7f3a0f7531d72536295c98a71a2f7a3c940124
NaveenAspire <naveenkumar.mani@aspiresys.com>
2022-05-05 18:41:32 +0530

    Worked for changes in pull data based on date

--- pull_data_from_meta_weather_api/pull_weather_information_from_metaweather_api.py
+++ pull_data_from_meta_weather_api/pull_weather_information_from_metaweather_api.py
@@ -41,7 +41,9 @@ class PullWeatherInformationFromMetaWeatherApi:
         )
         self.s_date = s_date
         self.e_date = e_date
-        self.last_run = get_date(config["pull_weather_information_from_metaweather_api"]["last_date"])
+        self.last_run = get_date(
+            config["pull_weather_information_from_metaweather_api"]["last_date"]
+        )
         self.path = os.path.join(parent_dir, config["local"]["local_file_path"], "meta_weather")
         os.makedirs(self.path, exist_ok=True)
         self.metaweather = MetaWeatherApi(logger)
@@ -50,19 +52,23 @@ class PullWeatherInformationFromMetaWeatherApi:
     def get_weather_information_for_given_dates(self):
         """This method will get the weather information from start date to end date"""
         check_date = datetime.now().date()
-        if self.s_date :
+        if self.s_date:
             if self.s_date < check_date:
                 response = self.get_weather_information_between_two_dates(self.s_date, self.e_date)
                 logging.info("weather information took for start date to end date")
-            print("Script ran for start and end date. So script was terminated without update last run..")
+            print(
+            "Script ran for start and end date. So script was terminated without update last run.."
+            )
             sys.exit()
         elif self.last_run and self.last_run < check_date:
             print(self.last_run)
-            response =self.get_weather_information_between_two_dates(self.last_run, check_date)
+            response = self.get_weather_information_between_two_dates(self.last_run, check_date)
             logging.info("weather information took for last run date to yesterday's date")
         else:
             data_exist = "Data available upto date..."
-            self.get_weather_information(check_date-timedelta(1)) if not self.last_run else print(data_exist)
+            self.get_weather_information(check_date - timedelta(1)) if not self.last_run else print(
+                data_exist
+            )
             response = None
             logging.info("weather information took for yesterday's date")
         return response
@@ -71,7 +77,7 @@ class PullWeatherInformationFromMetaWeatherApi:
         """This method will get the information between two dates of response"""
         try:
             dates = []
-            while start < end:
+            while start < end and start < datetime.now().date()-timedelta(1):
                 self.get_weather_information(start)
                 logging.info(f"weather information successfully took for {start}")
                 dates.append(start)
@@ -79,7 +85,7 @@ class PullWeatherInformationFromMetaWeatherApi:
         except Exception as err:
             print(err)
             dates = None
-        return dates    
+        return dates
 
     def get_weather_information(self, date):
         """This method used to get the woeid of city from api"""
@@ -91,10 +97,10 @@ class PullWeatherInformationFromMetaWeatherApi:
                     print("Script was terminated due error in api response..")
                     sys.exit()
                 self.get_given_date_response(response, key, date)
-                # break #--for testcase run 
-        except Exception as err :
+                # break #--for testcase run
+        except Exception as err:
             response = None
-        return response   
+        return response
 
     def get_given_date_response(self, response, city, date):
         """This method used to get only required date of information alone"""
@@ -102,7 +108,7 @@ class PullWeatherInformationFromMetaWeatherApi:
             date = date.strftime("%Y-%m-%d")
             res_df = pd.DataFrame(response)
             for i in range(24):
-                t_str = "{date}T{hour}".format(date=date, hour=str(i).zfill(2))
+                t_str = f"{date}T{str(i).zfill(2)}"
                 new_df = res_df[(res_df.created.str.contains(t_str))]
                 if not new_df.empty:
                     self.create_json_file(new_df, city, t_str)
@@ -115,17 +121,19 @@ class PullWeatherInformationFromMetaWeatherApi:
     def create_json_file(self, new_df, city, t_str):
         """This method is used for create the json file for the weather information
         record as same hour"""
-        try :
+        try:
             epoch = int(time())
             file_name = f"metaweather_{city}_{epoch}.json"
             new_df.to_json(self.path + "/" + file_name, orient="records", lines=True)
             key = self.get_partition_path(city, t_str) + "/" + file_name
             # self.upload_to_s3(self.path + "/" + file_name, key)
-            self.upload_to_dummy_s3(self.path + "/" + file_name, self.get_partition_path(city, t_str))
-            print(key +" is uploaded")
-            json_file_path = self.path+'/'+file_name
+            self.upload_to_dummy_s3(
+                self.path + "/" + file_name, self.get_partition_path(city, t_str)
+            )
+            print(key + " is uploaded")
+            json_file_path = self.path + "/" + file_name
         except Exception as err:
-            json_file_path =  None
+            json_file_path = None
         return json_file_path
 
     def upload_to_s3(self, file, key):
@@ -145,7 +153,8 @@ class PullWeatherInformationFromMetaWeatherApi:
         try:
             date_obj = datetime.strptime(t_str, "%Y-%m-%dT%H")
             partition_path = date_obj.strftime(
-                f"pt_city={city}/pt_year=%Y/pt_month=%m/pt_day=%d/pt_hour=%H/")
+                f"pt_city={city}/pt_year=%Y/pt_month=%m/pt_day=%d/pt_hour=%H/"
+            )
         except Exception as err:
             print(err)
             partition_path = None
