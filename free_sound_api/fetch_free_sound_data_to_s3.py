ef05c220b584c02135b54365dff2d210283b1d42
NaveenAspire <naveenkumar.mani@aspiresys.com>
2022-06-03 09:36:29 +0530

    Completed the free sound api task and uploaded with screenshots.

--- free_sound_api/fetch_free_sound_data_to_s3.py
+++ free_sound_api/fetch_free_sound_data_to_s3.py
@@ -3,10 +3,12 @@ sound api and upload those data as json in the s3 with partition"""
 
 import argparse
 import configparser
-from datetime import datetime, timedelta
+from datetime import datetime
 import os
+import shutil
 import sys
 from S3.s3 import S3Service
+from Temp_s3.temp_s3 import TempS3
 from logging_and_download_path import LoggingDownloadpath, parent_dir
 from free_sound_api import FreeSoundApi
 
@@ -15,87 +17,139 @@ config.read(os.path.join(parent_dir, "develop.ini"))
 logger_download = LoggingDownloadpath(config)
 logger = logger_download.set_logger("fetch_free_sound_api_data")
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--sound_id", type=str, help="Enter sound id for fetch similar sounds"
+)
+parser.add_argument("--username", type=str, help="Enter username for fetch user packs")
+parser.add_argument(
+    "endpoint",
+    choices=["sound_similar", "user_packs"],
+    help="Choose from choices for get single endpoint response either 'sound_similar' or 'user_packs'",
+)
+args = parser.parse_args()
+
+
 class FetchDataFromFreeSoundApi:
     """This class for fetching data from free sound api and
     upload the response as json file into s3 with partition"""
-    
+
     def __init__(self) -> None:
         """
         This is init method for the class FetchDataFromFreeSoundApi
-        
+
         Parameter :
-            """
+        """
         self.free_sound_api = FreeSoundApi(config)
         self.download_path = logger_download.set_downloadpath("free_sound_api")
-        self.s3_service = S3Service(logger)
-        self.similar_sound_path = config['fetch_free_sound_data']['similar_sound_bpath']
-        self.user_packs_path = config['fetch_free_sound_data']['user_packs_bpath']
-    
+        # self.s3_service = S3Service(logger)
+        self.temp_s3 = TempS3(config, logger)
+        self.similar_sound_path = config["fetch_free_sound_data"]["similar_sound_bpath"]
+        self.user_packs_path = config["fetch_free_sound_data"]["user_packs_bpath"]
+
     def fetch_similar_sounds(self, sound_id):
         """This method get the similar sound of the gieven sound id
-        
+
         Parameter :
             sound_id : The id of the sound"""
-        response = self.free_sound_api.similar_sounds(sound_id)
-        today = datetime.strftime(datetime.now().today().date(),"%Y%-m-%d")
-        if response :
-            create_json = self.create_json_file(response,f"{sound_id}.json")
-            key = os.path.join(self.similar_sound_path,self.get_partition(today),f"{sound_id}.json")
-            self.s3_service.upload_file(create_json,key)
-        
+        try:
+            response = self.free_sound_api.similar_sounds(sound_id)
+            logger.info("response successfuly get from similar sound endpoint...")
+            today = datetime.strftime(datetime.now().today().date(), "%Y-%m-%d")
+            if response is None:
+                sys.exit("Data does not get from api")
+            create_json = self.create_json_file(response, f"{sound_id}.json")
+            key = os.path.join(
+                self.similar_sound_path, self.get_partition(today), f"{sound_id}.json"
+            )
+            # self.s3_service.upload_file(create_json, key)
+            self.temp_s3.upload_local_s3(
+                create_json,
+                os.path.join(self.similar_sound_path, self.get_partition(today)),
+            )
+            # shutil.rmtree(self.download_path)
+            logger.info("Download Folder cleaned successfully...")
+        except Exception as err:
+            logger.exception(err)
+            print(err)
+        return True
+
     def fetch_user_packs(self, username):
         """This method get the packs which created by the user
-        
+
         Parameter :
             username : The username of the user"""
-        response = self.free_sound_api.user_packs(username)
-        if not response :
-            sys.exit("Data does not get from api")
-        start = min(response["created"])
-        while start <= max(response["created"]):
-            new_df = response[(response.created == start)]
-            if not new_df.empty:
-                create_json = self.create_json_file(new_df, start)
-                key = os.path.join(self.similar_sound_path,self.get_partition(start),f"{start}.json")
-                self.s3_service.upload_file(create_json,key)
-            start = start + timedelta(1)
-    
+        try:
+            response = self.free_sound_api.user_packs(username)
+            logger.info("response successfuly get from user packs endpoint...")
+            if response is None:
+                sys.exit("Data does not get from api")
+            unique_list = response.created.unique()
+            for unique in unique_list:
+                new_df = response[(response.created == unique)]
+                if not new_df.empty:
+                    file_name = f"{(unique.split('T')[0]).replace('-','')}.json"
+                    create_json = self.create_json_file(new_df, file_name)
+                    print(file_name)
+                    key = os.path.join(
+                        self.similar_sound_path,
+                        self.get_partition(unique.split("T")[0]),
+                        file_name,
+                    )
+
+                    # self.s3_service.upload_file(create_json,key)
+                    self.temp_s3.upload_local_s3(
+                        create_json,
+                        os.path.join(
+                            self.user_packs_path,
+                            self.get_partition(unique.split("T")[0]),
+                        ),
+                    )
+            shutil.rmtree(self.download_path)
+            logger.info("Download Folder cleaned successfully...")
+        except Exception as err:
+            logger.exception(err)
+            print(err)
+        return True
+
     def create_json_file(self, data_frame, file_name):
         """This method create the json file for the given dataframe as name given
         Parameter :
             data_frame : The dataframe need to be create as json file
             file_name : The name of the file which going to create"""
-        data_frame.to_json(os.path.join(self.download_path, file_name),
-                           orient="records",
-                           lines=True,)
-        file_path = os.path.join(self.download_path,file_name)
+        data_frame.to_json(
+            os.path.join(self.download_path, file_name),
+            orient="records",
+            lines=True,
+        )
+        file_path = os.path.join(self.download_path, file_name)
+        logger.info("Json file created successfully as name %s", file_name)
         return file_path
-        
-    def get_partition(self,partition_variable):
+
+    def get_partition(self, partition_variable):
         """This method will make the partition based on given date
         Parameter :
             partition_variable : The date that to be partition"""
         try:
             date_obj = datetime.strptime(partition_variable, "%Y-%m-%d")
-            partition_path = date_obj.strftime(
-                f"pt_year=%Y/pt_month=%m/pt_day=%d/"
-            )
+            partition_path = date_obj.strftime(f"pt_year=%Y/pt_month=%m/pt_day=%d/")
         except Exception as err:
             print(err)
-            partition_path = None
+            sys.exit(f"Script stopped due to {err}")
         return partition_path
-        
+
+
 def main():
     """This is the main method of this module"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--sound_id',type=int,help='Enter sound id for fetch similar sounds')
-    parser.add_argument('--username',type=int,help='Enter username for fetch user packs')
-    args = parser.parse_args()
     fetch_data = FetchDataFromFreeSoundApi()
-    fetch_data.fetch_similar_sounds(args.sound_id)
-    fetch_data.fetch_user_packs(args.username)
-    
+    if args.endpoint == "sound_similar" and args.sound_id:
+        fetch_data.fetch_similar_sounds(args.sound_id)
+    elif args.endpoint == "user_packs" and args.username:
+        fetch_data.fetch_user_packs(args.username)
+    else:
+        print("Please Give input for the Choosed endpoint...")
+
+
 if __name__ == "__main__":
     main()
-        
-        
--- free_sound_api/free_sound_api.py
+++ free_sound_api/free_sound_api.py
@@ -1,23 +1,31 @@
 """This module used for connecting api with the authentication
 and fetch the data based on the endpoint called"""
 
-import sys
+from cryptography.fernet import Fernet
 import requests
 import pandas as pd
 
+
 class FreeSoundApi:
     """This class for authenticate and access endpoint of the free sound api"""
-    
+
     def __init__(self, config_obj) -> None:
         """
         This is the init method for the class of FreeSoundApi
-        
+
         parameters:
             config : configParser object
         """
         self.config = config_obj
-        self.params = {'token' : self.config['free_sound_api']['access_token'],'page_size' : 100}
-    
+        fernet_key = self.config["free_sound_api"]["fernet_key"]
+        fernet = Fernet(fernet_key)
+        encrypted_key = bytes(self.config["free_sound_api"]["access_token"], "utf-8")
+        access_key = fernet.decrypt(encrypted_key).decode()
+        self.params = {
+            "token": access_key,
+            "page_size": 100,
+        }
+
     def similar_sounds(self, sound_id):
         """
         This method access enpoint for find similar sounds based on given sound id
@@ -26,45 +34,49 @@ class FreeSoundApi:
         Returns:
             dataframe : collection of json responses as dataframe.
         """
-        endpoint = self.config['free_sound_api']['similar_sound'].replace("<sound_id>",sound_id)
-        response = self.pagination(endpoint)    
+        endpoint = self.config["free_sound_api"]["similar_sound"].replace(
+            "<sound_id>", sound_id
+        )
+        response = pagination(endpoint, self.params)
         return response
-            
-        
+
     def user_packs(self, username):
         """This method access the endpoint for find packs which created by the user
         Parameter :
             username : The username of the user
         Returns :
             data_frame : collection json responses as dataframe"""
-        
-        endpoint = self.config['free_sound_api']['user_packs'].replace("<username>",username)
-        response = self.pagination(endpoint)
+
+        endpoint = self.config["free_sound_api"]["user_packs"].replace(
+            "<username>", username
+        )
+
+        response = pagination(endpoint, self.params)
         return response
-        
-    def pagination(self, endpoint,params):
-        """This method used to paginate the response and get the response as single dataframe
-        Parameter :
-            json_response : The first response of the endpoint
-            
-        Return :
-            data frame : Collection of json respose as single data frame"""
-        try :
-            dfs = []
-            while True:
-                response = requests.get(endpoint,params=params)
-                if response.status_code != 200 :
-                    raise Exception(f"Your endpoint '{endpoint}' returns status code '{response.status_code}'")
-                json_response = response.json()
-                data_frame = pd.DataFrame.from_records(json_response.get('results'))
-                dfs.append(data_frame)
-                endpoint = json_response.get("next")
-                if not endpoint:
-                    # The next link will not be availabe in the response
-                    print("There is no next link ...")
-                    break
-            data_frame = pd.concat(dfs)
-        except Exception as err :
-            print(f"Error occured : {err}")
-            data_frame = None
-        return data_frame
+
+def pagination(endpoint, params):
+    """This method used to paginate the response and get the response as single dataframe
+    Parameter :
+        json_response : The first response of the endpoint
+
+    Return :
+        data frame : Collection of json respose as single data frame"""
+    try:
+        dfs = []
+        while True:
+            response = requests.get(endpoint, params=params)
+            if response.status_code != 200:
+                raise Exception(
+                    f"Your endpoint '{endpoint}' returns status code '{response.status_code}'"
+                )
+            json_response = response.json()
+            data_frame = pd.DataFrame.from_records(json_response.get("results"))
+            dfs.append(data_frame)
+            endpoint = json_response.get("next")
+            if not endpoint:
+                break
+        data_frame = pd.concat(dfs)
+    except Exception as err:
+        print(f"Error occured : {err}")
+        data_frame = None
+    return data_frame
