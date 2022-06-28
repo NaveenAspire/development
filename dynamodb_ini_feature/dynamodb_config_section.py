201089c319dfefd8ee3ace93dfe846d71392871d
NaveenAspire <naveenkumar.mani@aspiresys.com>
2022-06-28 10:23:45 +0530

    Updated with methods for create table, put item, update item

--- dynamodb_ini_feature/dynamodb_config_section.py
+++ dynamodb_ini_feature/dynamodb_config_section.py
@@ -3,6 +3,15 @@ file and store section values into the table and use them in script"""
 
 import argparse
 import ast
+import configparser
+from dynamodb import DynamoDB
+from logging_and_download_path import LoggingDownloadpath,parent_dir
+import os
+
+config = configparser.ConfigParser()
+config.read(os.path.join(parent_dir, "develop.ini"))
+logger_download = LoggingDownloadpath(config)
+logger = logger_download.set_logger("fetch_ebird_api_data")
 
 class DynamodbConfig:
     """This is the class used to create the table for
@@ -10,7 +19,22 @@ class DynamodbConfig:
     
     def __init__(self) -> None:
         """This is the init method for the class DynamodbConfig"""
-        pass
+        self.dynamodb = DynamoDB(config)
+    
+    def dynamodb_create_table(self,params):
+        """This is the method to create the dynamodb table for the user given params"""
+        table = self.dynamodb.create_table(params)
+        print(table)
+        
+    def dynamodb_put_item_to_table(self,table_name,params):
+        """This is method used to insert the item into the dynamodb table"""
+        item_response = self.dynamodb.put_item(table_name,params)
+        print(item_response)
+        
+    def update_item_to_table(self,table_name,params):
+        """This method used to update the item into the dynamodb table"""
+        update_response = self.dynamodb.update_item(table_name,params)
+        print(update_response)
 
 
 def main():
@@ -24,12 +48,14 @@ def main():
     put_item = subparsers.add_parser('put_item')
     put_item.add_argument("table",type=str)
     put_item.add_argument("params",type=ast.literal_eval)
-    get_item = subparsers.add_parser('get_item',type=str)
-    get_item.add_argument('table')
+    get_item = subparsers.add_parser('get_item')
+    get_item.add_argument('table',type=str)
     get_item.add_argument('params',type=ast.literal_eval)
+    update_item = subparsers.add_parser('update_item')
+    update_item.add_argument('table',type=str)
+    update_item.add_argument('params',type=ast.literal_eval)
     args = parser.parse_args()
     
-    print(type(args.params))
 
 if __name__ == "__main__":
     main()
